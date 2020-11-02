cf4617f4a87622aba2ef2996a053b2bfc12389f9
Honorhero <38943474+Honorhero@users.noreply.github.com>
2020-11-02 23:43:45 +0800

    public文件夹
    vue静态的文件、图片和入口

---
+++ public/static/config.js
@@ -0,0 +1,4 @@
+window.apiHost = "http://lib.xmzhzm.cn"; // 接口地址
+// window.userApiHost = "http://81.68.174.253:51680"; // 用户登录、注册等接口地址
+// window.userApiHost = "http://lib.xmzhzm.cn"; // 用户登录、注册等接口地址
+// window.examApiHost = "http://81.68.174.253:51682"; // 后台考试系统地址
